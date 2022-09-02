e4fe23a1013ab0a54247684c56c7878e5c9586ed
fidan2004 <110298605+fidan2004@users.noreply.github.com>
2022-09-02 21:26:51 +0400

    Add files via upload

---
+++ taask4/taask4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace taask4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessToKill(new List<string>
+            {
+                "writer.exe",
+                "Explorer.exe",
+                "information.exe"
+            });
+        }
+
+        public static List<string> ProcessToKill(List<string> process)
+        {
+            List<string> processToKill = new List<string>(3);
+
+            Console.WriteLine(string.Format("Capacity {0}", processToKill.Capacity));
+
+            Console.WriteLine(string.Format("Count {0}", processToKill.Count));
+
+            foreach (var item in process)
+            {
+                if (item != "Explorer.exe")
+                {
+                    processToKill.Add(item);
+                }
+            }
+
+            foreach (var p in processToKill)
+            {
+                Console.WriteLine(p);
+            }
+
+            return processToKill;
+        }
+    }
+}
